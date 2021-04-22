194fee36e00945c5928ee723f665d11d064d4a2a
Giyu <80783085+c1am@users.noreply.github.com>
2021-04-21 23:44:27 -0400

    final code upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", createPassword);
 
@@ -124,53 +123,3 @@ function createPassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-=======
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// Function to generate the password
-function generatePassword() {
-  var possiblePasswords = getPossiblePasswords();
-  var resultingPassword = ();
-  var possibleCharacters = ();
-  var definiteCharacters = ();
-
-  if (possiblePasswords.hasSpecialCharacter) {
-    possibleCharacters = possibleCharacters.concat(specialCharacters);
-    definiteCharacters.push(getRandom(specialCharacters));
-  }
-  
-  if (possiblePasswords.hasNumericCharacters) {
-    possibleCharacters = possibleCharacters.concat(numericCharacters);
-    definiteCharacters.push(getRandom(numericCharacters));
-  }
-
-  if (possiblePasswords.hasLowerCaseCharacters) {
-    possibleCharacters = possibleCharacters.concat(lowerCaseCharacters);
-    definiteCharacters.push(getRandom(lowerCaseCharacters));
-  }
-
-  if (possiblePasswords.hasUpperCaseCharacters) {
-    possibleCharacters = possibleCharacters.concat(upperCaseCharacters);
-    definiteCharacters.push(getRandom(upperCaseCharacters));
-  }
-
-  for (var i=0; i<definiteCharacters.length; i++) {
-    resultingPassword[i] = definiteCharacters();
-  }
-
-  return resultingPassword;
-}
->>>>>>> 837b9232dae543fda19a1aafa1004cd818d8f97a
